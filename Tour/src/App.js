4aeb0ad341b69e32bcf90aae84a833de4d03dc65
Yash <yash1997bm@gmail.com>
2021-03-05 17:56:36 +0530

    tour

---
+++ Tour/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Display from "./components/Display";
+
+const App = () => {
+  return (
+    <>
+      <Display />
+    </>
+  );
+};
+
+export default App;
